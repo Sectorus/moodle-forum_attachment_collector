1fb2d505b9a5aeef4602be84c8bb43f6d6948dda
Sectorus <stephan.lorbek@uni-graz.at>
2020-08-28 12:57:30 +0200

    Update exporter.php
    Added check for attachment count

--- views/exporter.php
+++ views/exporter.php
@@ -53,7 +53,7 @@ if(isset($_GET['selectCourse']) && !isset($_GET['selectForum'])){
 	echo '<select name = "selectForum">';
 	foreach ($all_forums as $forum){
 		$attachment_count = sizeof(getFiles($forum->id));
-		echo '<option value ="' . $forum->id . '">' . $forum->name . ' (' . $attachment_count . " " . get_string('files', 'local_forum_attachment_collector') . ')</option>';
+		if($attachment_count > 0) echo '<option value ="' . $forum->id . '">' . $forum->name . ' (' . $attachment_count . " " . get_string('files', 'local_forum_attachment_collector') . ')</option>';
 	}
 	echo '<input type = "submit" name="submit" class="btn-download" value="';
 	echo get_string('expbtn', 'local_forum_attachment_collector') . '" />';
