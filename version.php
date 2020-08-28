2f4d8352d12c7e0dbfbdf80b28069a3767ac1e51
Sectorus <stephan.lorbek@uni-graz.at>
2020-08-28 11:57:18 +0200

    Update version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version   = 2020082200;
-$plugin->release   = '1.0.0';
+$plugin->release   = '1.1.0';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2019111800; // Moodle 3.8 release and upwards.
 $plugin->component = 'local_forum_attachment_collector';
